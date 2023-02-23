0b9561253543ac98b3e3c1a5d3f00db8771ba0fe
Sai Ganesh Kodipelly <ganeshrao9949@gmail.com>
2023-02-23 18:05:16 +0530

    Changed UI

--- src/Audio.js
+++ src/Audio.js
@@ -1,27 +1,10 @@
-import { SoligaPaths, LambaniPaths } from "./path"
 import Transcript from "./Transcript"
 
 const DisplayFiles = ({ src }) => {
-    const data = src === "soliga" ? SoligaPaths : LambaniPaths
-
     return (
         <>
             <h2>{src === "soliga" ? "Soliga TTS Results" : "Lambani TTS Results"}</h2>
             <Transcript src={src} />
-            <div className="row">
-                <div className="col">
-                    <h3>Original</h3>
-                    {data.original.map((audio, index) => <audio key={index} src={audio} controls />)}
-                </div>
-                <div className="col">
-                    <h3>Hifi</h3>
-                    {data.hifi.map((audio, index) => <audio key={index} src={audio} controls />)}
-                </div>
-                <div className="col">
-                    <h3>Waveglow</h3>
-                    {data.waveglow.map((audio, index) => <audio key={index} src={audio} controls />)}
-                </div>
-            </div>
         </>
     )
 }
--- src/Transcript.js
+++ src/Transcript.js
@@ -2,11 +2,27 @@ import { SoligaPaths, LambaniPaths } from "./path"
 
 const Transcript = ({ src }) => {
     const transcripts = src === "soliga" ? SoligaPaths.transcript : LambaniPaths.transcript
+    const data = src === "soliga" ? SoligaPaths : LambaniPaths
 
     return (
         <div className="mb-4">
-            <h4>Transcript</h4>
-            {transcripts.map((transcript, index) => <p key={index} className="m-0">{transcript}</p>)}
+            <div className="row my-3">
+                <div className="col"><h3>Original</h3></div>
+                <div className="col"><h3>hifi-Gan</h3></div>
+                <div className="col"><h3>Waveglow</h3></div>
+            </div>
+            {
+                transcripts.map((transcript, index) =>
+                    <div className="my-2">
+                        <p key={index} className="m-1">{transcript}</p>
+                        <div className="row">
+                            <div className="col"><audio key={index} src={data.original[index]} controls /></div>
+                            <div className="col"><audio key={index} src={data.hifi[index]} controls /></div>
+                            <div className="col"><audio key={index} src={data.waveglow[index]} controls /></div>
+                        </div>
+                    </div>
+                )
+            }
         </div>
     )
 }
